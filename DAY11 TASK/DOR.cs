e446fb07d451fbea2d562804994d9cc78690915b
Meejuru Ramkishore <55176924+Ramkishore045@users.noreply.github.com>
2022-03-21 16:54:44 +0530

    Add files via upload

---
+++ DAY11 TASK/DOR.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+using System.IO;
+
+namespace ConsoleApp1
+{
+    class DOR
+    {
+        public static void Main()
+        {
+            Queue q = new Queue();
+            q.Enqueue("Ice cream");
+            q.Enqueue("Falooda");
+            q.Enqueue("Lassi");
+            q.Enqueue("Gulab jamuns");
+            q.Enqueue("Coconut Truffle");
+            q.Enqueue("Rasamalai");
+            q.Enqueue("Caramel Custard");
+            q.Enqueue("Lichi Mousse");
+            FileStream fs = new FileStream("E:\\softura training\\DAY10\\DAY11 TASK\\DOR.txt", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            BinaryWriter br = new BinaryWriter(fs);
+            foreach (string cal in q)
+            {
+                br.Write("\n" + cal);
+            }
+            br.Flush();
+            fs.Close();
+            FileInfo fs1 = new FileInfo("E:\\softura training\\DAY10\\DAY11 TASK\\DOR.txt");
+            Console.WriteLine(fs1.Length);
+            Console.WriteLine(fs1.CreationTime);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ DAY11 TASK/Task2.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Collections;
+namespace ConsoleApp1
+{
+    class Task2
+    {
+        public static void Main()
+
+        {
+            FileStream fs = new FileStream("E:\\softura training\\DAY10\\DAY11 TASK\\det.txt",FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            BinaryWriter bw = new BinaryWriter(fs);
+            BinaryWriter bw1 = new BinaryWriter(fs);
+            BinaryWriter bw2 = new BinaryWriter(fs);
+            BinaryWriter bw3 = new BinaryWriter(fs);
+            bw.Write("ASP.NET\n");
+            bw1.Write("RAMKISHORE\n");
+            bw2.Write("21\n");
+            bw3.Write("Male\n");
+            bw.Flush();
+            bw1.Flush();
+            bw2.Flush();
+            bw3.Flush();
+            fs.Close();
+            FileStream fs1 = new FileStream(@"E:\\softura training\\DAY10\\DAY11 TASK\\det.txt", FileMode.Open, FileAccess.ReadWrite);
+            BinaryReader br = new BinaryReader(fs1);
+            BinaryReader br1 = new BinaryReader(fs1);
+            BinaryReader br2 = new BinaryReader(fs1);
+            BinaryReader br3 = new BinaryReader(fs1);
+
+            string str = br.ReadString();
+            string str1 = br1.ReadString();
+            string str2 = br2.ReadString();
+            string str3 = br3.ReadString();
+    
+            Console.WriteLine(str);
+            Console.WriteLine(str1);
+            Console.WriteLine(str2);
+            Console.WriteLine(str3);
+            fs1.Close();
+            Console.Read();
+        }
+
+
+    }
+}
