ae663eeb445ceb6b4e6c53ebe1e920327fcb91e0
Meejuru Ramkishore <55176924+Ramkishore045@users.noreply.github.com>
2022-03-22 16:19:27 +0530

    Add files via upload

---
+++ Day 12 Task/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+
+
+namespace day12
+
+{
+    [Serializable]
+    class Employeedets
+    {
+
+        public string name = "Sid the great";
+        public int age = 25;
+        public string Empid = "ABC$321";
+        public string gender = "Male";
+
+    }
+    class Program
+    {
+        public void SeretoFile()
+        {
+            Employeedets sobj = new Employeedets();
+            FileStream fs = new FileStream("Empdets.txt", FileMode.OpenOrCreate, FileAccess.Write);
+            BinaryFormatter f = new BinaryFormatter();
+            f.Serialize(fs, sobj);
+            fs.Close();
+        }
+        public void Dserial()
+        {
+            FileStream fs = new FileStream("Empdets.txt", FileMode.Open, FileAccess.Read);
+            BinaryFormatter f = new BinaryFormatter();
+            Employeedets stu = (Employeedets)f.Deserialize(fs);
+            Console.WriteLine(stu.name);
+            Console.WriteLine(stu.age);
+            Console.WriteLine(stu.gender);
+            Console.WriteLine(stu.Empid);
+
+
+
+        }
+
+        public static void Main()
+        {
+            Program lobj = new Program();
+            lobj.SeretoFile();
+            lobj.Dserial();
+            Console.ReadLine();
+
+        }
+    }
+}
