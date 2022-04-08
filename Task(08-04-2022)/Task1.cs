5d4e6c2db494c691fd9aebec335daa2455afac3a
Meejuru Ramkishore <55176924+Ramkishore045@users.noreply.github.com>
2022-04-08 17:39:47 +0530

    Add files via upload

---
+++ Task(08-04-2022)/Task1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Linq
+{
+    class Task1
+    {
+		public static void Main()
+		{
+			int eye = 52, Legs = 80;
+
+			int count = 0;
+			int count2 = 0;
+
+			count = 2 * (eye) - (Legs);
+			count = count / 2;
+
+			Console.WriteLine("No of Ducks:" + count);
+
+			count2 = (eye) / 2 - count;
+			Console.WriteLine("No of Goats:" + count2);
+		}
+	}
+}
